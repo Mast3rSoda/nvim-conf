ee353d099f1e67d2c6b3bfbdf64d95b07d30fe21
Mast3rSoda <mast3rsoda@gmail.com>
2024-03-03 09:03:52 +0100

    added lsp_signature

--- lua/modules/plugins/lsp.lua
+++ lua/modules/plugins/lsp.lua
@@ -1,23 +1,28 @@
 return {
-{'williamboman/mason.nvim', config = require("modules.configs.lsp.mason")},
-{'williamboman/mason-lspconfig.nvim', config = require("modules.configs.lsp.mason-lspconfig")},
-{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x', config = require("modules.configs.lsp.lsp-z")},
-{'neovim/nvim-lspconfig'},
-{'hrsh7th/cmp-nvim-lsp'},
-{'hrsh7th/nvim-cmp', config = require("modules.configs.lsp.cmp")},
-{'L3MON4D3/LuaSnip', config = require("modules.configs.lsp.luasnip"), dependencies = { "rafamadriz/friendly-snippets" }},
-{"saadparwaiz1/cmp_luasnip"},
-{ "rafamadriz/friendly-snippets" },
-{ 
-    "danymat/neogen", 
-    dependencies = "nvim-treesitter/nvim-treesitter", 
-    config = true,
-    -- Uncomment next line if you want to follow only stable versions
-    -- version = "*" 
-},
--- { "dense-analysis/ale",
---     config = function()
-        
---     end }
+        { 'williamboman/mason.nvim',           config = require("modules.configs.lsp.mason") },
+        { 'williamboman/mason-lspconfig.nvim', config = require("modules.configs.lsp.mason-lspconfig") },
+        { 'VonHeikemen/lsp-zero.nvim',         branch = 'v3.x',                                        config = require("modules.configs.lsp.lsp-z") },
+        { 'neovim/nvim-lspconfig' },
+        { 'hrsh7th/cmp-nvim-lsp' },
+        { 'hrsh7th/nvim-cmp',                  config = require("modules.configs.lsp.cmp") },
+        { 'L3MON4D3/LuaSnip',                  config = require("modules.configs.lsp.luasnip"),        dependencies = { "rafamadriz/friendly-snippets" } },
+        { "saadparwaiz1/cmp_luasnip" },
+        { "rafamadriz/friendly-snippets" },
+        {
+            "danymat/neogen",
+            dependencies = "nvim-treesitter/nvim-treesitter",
+            config = true,
+            -- Uncomment next line if you want to follow only stable versions
+            -- version = "*"
+        },
+        -- { "dense-analysis/ale",
+        --     config = function()
 
-}
+        --     end }
+
+    {
+        "ray-x/lsp_signature.nvim",
+        event = "VeryLazy",
+        config = require("modules.configs.lsp.lsp_signature"),
+    },
+    }
