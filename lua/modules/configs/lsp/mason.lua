7c4187d92e5006f0422967b54fbc7e5bbecf47b1
Mast3rSoda <soda@Soda>
2024-08-16 12:06:58 +0200

    removed mason registry provider

--- lua/modules/configs/lsp/mason.lua
+++ lua/modules/configs/lsp/mason.lua
@@ -11,7 +11,24 @@ return function()
                 -- package_uninstalled = "✗"
 
             }
-        }
+        },
+        registries = {
+            "github:mason-org/mason-registry",
+        },
+
+        ---@since 1.0.0
+        -- The provider implementations to use for resolving supplementary package metadata (e.g., all available versions).
+        -- Accepts multiple entries, where later entries will be used as fallback should prior providers fail.
+        -- Builtin providers are:
+        --   - mason.providers.registry-api  - uses the https://api.mason-registry.dev API
+        --   - mason.providers.client        - uses only client-side tooling to resolve metadata
+        providers = {
+            -- NOTE: uncomment once the mason registry comes back to life
+            -- "mason.providers.registry-api",
+            "mason.providers.client",
+        },
+
+
     })
 end
 
