e7abcb67c9d7553e6dd6853586b3bf54725104f8
Mast3rSoda <mast3rsoda@gmail.com>
2025-03-15 09:48:37 +0100

    removed unused plugins

--- lua/modules/configs/lsp/nvim-lint.lua
+++
@@ -1,8 +0,0 @@
--- return function ()
---     require('lint').linters_by_ft = {
---         c = {"clangtidy"},
---         cpp = {"clangtidy"},
---         go = {"golangci-lint"},
-
--- }
--- end
--- lua/modules/plugins/lsp.lua
+++ lua/modules/plugins/lsp.lua
@@ -24,7 +24,7 @@ return {
             "saadparwaiz1/cmp_luasnip",
             'hrsh7th/cmp-nvim-lsp',
             "hrsh7th/cmp-buffer",
-                "hrsh7th/cmp-cmdline",
+            "hrsh7th/cmp-cmdline",
         },
         config = require("modules.configs.lsp.cmp"),
     },
