eaa59f326d0f333fd80536bf5a866d26b59e24ce
Mast3rSoda <mast3rsoda@gmail.com>
2025-04-05 16:53:46 +0200

    removed omnisharp from mason always install

--- lua/modules/configs/lsp/mason-lspconfig.lua
+++ lua/modules/configs/lsp/mason-lspconfig.lua
@@ -1,7 +1,7 @@
 return function()
     require("lsp-zero").extend_lspconfig()
     require("mason-lspconfig").setup({
-        ensure_installed = { "clangd", "omnisharp_mono", "gopls", "lua_ls" },
+        ensure_installed = { "clangd", "gopls", "lua_ls" },
         handlers = {
             function(server_name)
                 require('lspconfig')[server_name].setup({})
